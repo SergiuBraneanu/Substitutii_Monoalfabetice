834d6b185b76de6aa8394402390638749cfb1fd2
Sergiu <Sergiu@LAPTOP-UH2BJPH9>
2020-10-25 15:11:28 +0200

    criptare

--- Substitutii Monoalfabetice/Program.cs
+++ Substitutii Monoalfabetice/Program.cs
@@ -6,11 +6,26 @@ namespace Substitutii_Monoalfabetice
     {
         static void Main(string[] args)
         {
+            Cifrul_lui_Cezar c = new Cifrul_lui_Cezar();
+            Cifrul_lui_Cezar_generalizat cg = new Cifrul_lui_Cezar_generalizat();
+            ROT13 rot = new ROT13();
+            Substitutie_Monoalfabetica sm = new Substitutie_Monoalfabetica();
             Console.Write("Introduceti textul:");
             string text = Console.ReadLine();
+            Console.Write("Introduceti cheia pt cifrul generalizat:");
+            int cheiecezargen = int.Parse(Console.ReadLine());
             int cheiecezar = 3;
-            Cifrul_lui_Cezar c = new Cifrul_lui_Cezar();
+            int cheierot = 13;
+            
+
+            Console.Write("Textul criptat cu cifrul lui Cezar:");
             c.Criptare(text, cheiecezar);
+            Console.Write("Textul criptat cu cifrul lui Cezar generalizat:");
+            cg.Criptare(text, cheiecezargen);
+            Console.Write("Textul criptat cu cifrul ROT13:");
+            rot.Criptare(text, cheierot);
+            Console.Write("Textul criptat prin substitutie monoalfabetica:");
+            sm.Criptare(text, 0);
         }
     }
 }
---
+++ Substitutii Monoalfabetice/Substitutie Monoalfabetica.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Substitutii_Monoalfabetice
+{
+    class Substitutie_Monoalfabetica:IAlgoritmSimetric
+    {
+        public void Criptare(string text,int cheie)
+        {
+            StringBuilder textaux = new StringBuilder();
+            foreach(char c in text)
+            {
+                if (char.IsLetter(c)) textaux.Append((char)Schimbare(c));
+                else textaux.Append(c);
+            }
+            Console.WriteLine(textaux);
+        }
+
+        private int Schimbare(char c)
+        {
+            switch(c)
+            {
+                case 'a':
+                    c = 'v';
+                    break;
+                case 'b':
+                    c = 'k';
+                    break;
+                case 'c':
+                    c = 'p';
+                    break;
+                case 'd':
+                    c = 'q';
+                    break;
+                case 'e':
+                    c = 'y';
+                    break;
+                case 'f':
+                    c = 'o';
+                    break;
+                case 'g':
+                    c = 'l';
+                    break;
+                case 'h':
+                    c = 'w';
+                    break;
+                case 'i':
+                    c = 'z';
+                    break;
+                case 'j':
+                    c = 'c';
+                    break;
+                case 'k':
+                    c = 'i';
+                    break;
+                case 'l':
+                    c = 'a';
+                    break;
+                case 'm':
+                    c = 't';
+                    break;
+                case 'n':
+                    c = 'b';
+                    break;
+                case 'o':
+                    c = 's';
+                    break;
+                case 'p':
+                    c = 'j';
+                    break;
+                case 'q':
+                    c = 'x';
+                    break;
+                case 'r':
+                    c = 'e';
+                    break;
+                case 's':
+                    c = 'n';
+                    break;
+                case 't':
+                    c = 'd';
+                    break;
+                case 'u':
+                    c = 'f';
+                    break;
+                case 'v':
+                    c = 'u';
+                    break;
+                case 'w':
+                    c = 'h';
+                    break;
+                case 'x':
+                    c = 'g';
+                    break;
+                case 'y':
+                    c = 'm';
+                    break;
+                case 'z':
+                    c = 'a';
+                    break;
+            }
+            return c;
+
+        }
+    }
+}
